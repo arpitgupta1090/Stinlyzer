a93054915ac3413846112f2bbc782667b1a4cecf
Arpit_Gupta <arpitgupta1090@gmail.com>
2022-01-18 16:43:34 +0530

    swagger and redoc urls changed

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from routers import transaction, user, stock
 from fastapi.openapi.utils import get_openapi
 from custom_exceptions.exception import DuplicateUserException, duplicate_user_exception
 
-app = FastAPI()
+app = FastAPI(docs_url="/", redoc_url="/docs")
 
 models.Base.metadata.create_all(engine)
 
-app.include_router(transaction.router)
 app.include_router(user.router)
 app.include_router(stock.router)
+app.include_router(transaction.router)
 
 app.add_exception_handler(DuplicateUserException, duplicate_user_exception)
 
